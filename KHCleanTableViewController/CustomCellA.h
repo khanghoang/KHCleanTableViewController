3ea47000b67526a30370d51a3a81d2bc7f393c80
khanghoang <hoangtrieukhang@gmail.com>
2014-10-03 14:32:45 +0700

    Add custom cell A

---
+++ KHCleanTableViewController/CustomCellA.h
@@ -0,0 +1,13 @@
+//
+//  CustomCellA.h
+//  KHCleanTableViewController
+//
+//  Created by Triệu Khang on 3/10/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCellA : UITableViewCell
+
+@end
---
+++ KHCleanTableViewController/CustomCellA.m
@@ -0,0 +1,13 @@
+//
+//  CustomCellA.m
+//  KHCleanTableViewController
+//
+//  Created by Triệu Khang on 3/10/14.
+//  Copyright (c) 2014 Triệu Khang. All rights reserved.
+//
+
+#import "CustomCellA.h"
+
+@implementation CustomCellA
+
+@end
